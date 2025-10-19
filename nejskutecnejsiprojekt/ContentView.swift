9d9b7e4fe33ee308d619e254b68ab8dea504fa47
Jaroslav Bělák <jarca0123@gmail.com>
2025-10-19 11:11:47 +0200

    Initial Commit

---
+++ nejskutecnejsiprojekt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nejskutecnejsiprojekt
+//
+//  Created by Jaroslav Bělák on 19.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nejskutecnejsiprojekt/nejskutecnejsiprojektApp.swift
@@ -0,0 +1,17 @@
+//
+//  nejskutecnejsiprojektApp.swift
+//  nejskutecnejsiprojekt
+//
+//  Created by Jaroslav Bělák on 19.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct nejskutecnejsiprojektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
